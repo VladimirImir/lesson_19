26aac85fa95d0db81a6e448c29eced34a8b80ca0
VladimirImir <mimimvvv@gmail.com>
2023-06-08 21:57:35 +0300

    19.13 - Set.

---
+++ src/com/dev/lesson19/set/SetExample.java
@@ -0,0 +1,28 @@
+package com.dev.lesson19.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetExample {
+
+    public static void main(String[] args) {
+        /** HashSet */
+        /** LinkedHashSet<>() - сохраняет порядок. */
+        /** TreeSet<>() - сохраняет в алфавитном порядке. */
+        /** Для сортировки нужно использовать только TreeSet. */
+        Set<String> set = new TreeSet<>();
+        System.out.println(set.add("ddd"));
+        System.out.println(set.add("aaa"));
+        System.out.println(set.add("bbb"));
+        System.out.println(set.add("ccc"));
+        System.out.println(set.add("bbb"));
+
+        System.out.println(set);
+
+        for (String value : set) {
+            System.out.println(value);
+        }
+    }
+}
